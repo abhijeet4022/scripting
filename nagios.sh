76f308cb45fd773fd981dc0c08d0dced138a49f8
abhijeet <abhijeet4022@gmail.com>
2022-10-01 16:42:54 +0530

    Revert "Git configuration Coding is not working properly"
    This reverts commit b8f1bc6fea6fc6799356af9572ffd6ff7419d0bd.

--- nagios.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yum install -y httpd httpd-tools php gcc glibc glibc-common gd gd-devel make net-snmp openssl openssl-devel unzip wget perl
-useradd nagios
-groupadd nagcmd
-usermod -G nagcmd nagios
-usermod -G nagcmd apache
-mkdir /root/nagios
